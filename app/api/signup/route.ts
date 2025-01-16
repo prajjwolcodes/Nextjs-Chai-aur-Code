1a05463a946e31ed04b14a1e2937c4b03356b042
prajjwolcodes <shresthaprajjwol4@gmail.com>
2025-01-16 15:49:25 +0545

    Signup controller

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,34 +1,66 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import { sendVerificationEmail } from "../../helpers/sendVerificationEmail";
 import dbConnect from "../../lib/dbConnect";
 import UserModel, { User } from "../../models/userModel";
 import bcrypt from "bcryptjs"
+import { NextRequest } from "next/server";
 
-
-
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+export async function POST(req: NextRequest) {
     await dbConnect()
-
     try {
-        const { email, username, password }: User = await req.body;
+        const { email, username, password } = await req.json();
+        const existingUserByEmail = await UserModel.findOne({ email })
+        console.log(existingUserByEmail)
+        if (existingUserByEmail)
+            return Response.json({
+                message: "This email is already taken"
+            })
 
-        const user = await UserModel.create({
+        const existingUserByUsername = await UserModel.findOne({ username, isVerfied: true })
+        if (existingUserByUsername)
+            return Response.json({
+                message: "This username is already taken"
+            }, {
+                status: 400
+            })
+
+        console.log(email, username, password)
+
+        const verifyCode = Math.floor(Math.random() * 900000) + 100000;
+        const verifyCodeExpiry = new Date()
+        verifyCodeExpiry.setHours(verifyCodeExpiry.getHours() + 1)
+        const hashedPassword = bcrypt.hashSync(password, 10)
+
+        const newUser = new UserModel({
             email, username,
-            password: bcrypt.hash(10, password)
+            password: hashedPassword,
+            verifyCode,
+            verifyCodeExpiry,
+            messages: []
+
         })
 
-        return res.json({
+        await newUser.save()
+
+        return Response.json({
             message: "User Succesfully Created",
-            user: user
+            // user: newUser
         })
 
     } catch (error) {
-        console.log("Error in Signing up a user", error)
+        console.error("Error in Signing up a user", error);
+        return Response.json({
+            message: "Internal Server Error",
+            error: error.message,
+        });
     }
-
-
 
     // sendVerificationEmail(email, username, verifyCode);
 }
 
 
+export async function GET(req: NextRequest) {
+    const users = await UserModel.find()
+    return Response.json({ users })
+
+
+}
