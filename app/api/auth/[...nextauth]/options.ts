90b47a2ef40866cd3694afa7f8767b9ae171f4fe
Prajjwol | Shrestha <132867834+prajjwolcodes@users.noreply.github.com>
2025-01-29 13:16:35 +0545

    Add next auth google provider

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,3 +1,19 @@
+//For the implementation of other providers:
+// Google:
+
+// GoogleProvider({
+//     clientId: process.env.GOOGLE_CLIENT_ID,
+//     clientSecret: process.env.GOOGLE_CLIENT_SECRET
+//   })
+
+// get the clientId and client Secret from google cloud console.
+// 1. Open the project
+// 2. Go to api and services -- Oauth consent screen -- fill all the details -- save
+// 3. Come back to credentials.
+// 4. Create Oauth clientId -- leave all as it is and add Authorized redirect URIs as "http://localhost:3000/api/auth/callback/google"
+// 5. Call the signIn("google") function in client side (no await needed).
+
+
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import dbConnect from "../../../lib/dbConnect";
